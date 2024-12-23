f9345e44dcefe7a97a431b49f2b7c592b25e5303
Yathin-B <yathinbn56@gmail.com>
2024-12-23 11:31:07 +0530

    Add files via upload

---
+++ LearnGit/LearnGit/LearnGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnGitApp.swift
+//  LearnGit
+//
+//  Created by Yathin B N on 20/12/24.
+//
+
+import SwiftUI
+
+@main
+struct LearnGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
